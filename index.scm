bded0178612c464628c6d830764872c22d3d8d1d
Justin Ethier <justin.ethier@gmail.com>
2019-07-18 18:31:07 -0400

    Added (cyclone array-list)

--- index.scm
+++ index.scm
@@ -1,3 +1,6 @@
 (cyclone-packages
  ((cyclone iset)
-  (0.1 "https://raw.githubusercontent.com/arthurmaciel/cyclone-iset/master/cyclone/package.scm" "https://github.com/arthurmaciel/cyclone-iset/archive/master.tar.gz" "988dce15de82a2989b0d9a40801a6066aa31ce934e718cb92a50f2eeb3e00064")))
+  (0.1 "https://raw.githubusercontent.com/arthurmaciel/cyclone-iset/master/cyclone/package.scm" "https://github.com/arthurmaciel/cyclone-iset/archive/master.tar.gz" "988dce15de82a2989b0d9a40801a6066aa31ce934e718cb92a50f2eeb3e00064"))
+ ((cyclone array-list)
+  (0.1 "https://raw.githubusercontent.com/cyclone-scheme/array-list/master/package.scm" "https://github.com/cyclone-scheme/array-list/archive/master.tar.gz" "63fc4f7176b4499a43d6d3fc24eb305c24e89a575c99c4d61bd4ae34046134f4"))
+)
