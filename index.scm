97ed6dd197307eb2648b1c72aba8bf49314256ec
Justin Ethier <justin.ethier@gmail.com>
2019-07-18 18:35:24 -0400

    Relocated package.scm for (cyclone array-list)

--- index.scm
+++ index.scm
@@ -2,5 +2,5 @@
  ((cyclone iset)
   (0.1 "https://raw.githubusercontent.com/arthurmaciel/cyclone-iset/master/cyclone/package.scm" "https://github.com/arthurmaciel/cyclone-iset/archive/master.tar.gz" "988dce15de82a2989b0d9a40801a6066aa31ce934e718cb92a50f2eeb3e00064"))
  ((cyclone array-list)
-  (0.1 "https://raw.githubusercontent.com/cyclone-scheme/array-list/master/package.scm" "https://github.com/cyclone-scheme/array-list/archive/master.tar.gz" "63fc4f7176b4499a43d6d3fc24eb305c24e89a575c99c4d61bd4ae34046134f4"))
+  (0.1 "https://raw.githubusercontent.com/cyclone-scheme/array-list/master/cyclone/package.scm" "https://github.com/cyclone-scheme/array-list/archive/master.tar.gz" "eccdc6c7d70e44cefd97dff800f83046a1399fbd8c7a06e4b579f3a9dda5378d"))
 )
