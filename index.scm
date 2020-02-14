aa0cf74072965cf2005b7f183aaedb2592286ee4
Justin Ethier <justin.ethier@gmail.com>
2020-02-14 13:08:12 -0500

    Update package index to point to official array-list release

--- index.scm
+++ index.scm
@@ -2,5 +2,5 @@
  ((cyclone iset)
   (0.1 "https://raw.githubusercontent.com/arthurmaciel/cyclone-iset/master/package.scm" "https://github.com/arthurmaciel/cyclone-iset/archive/master.tar.gz" "6e7c5bc2e5aede85822ccfc0951e694321f0cc54841062397546c3d92460bb2b"))
  ((cyclone array-list)
-  (0.1 "https://raw.githubusercontent.com/cyclone-scheme/array-list/master/package.scm" "https://github.com/cyclone-scheme/array-list/archive/master.tar.gz" "8699103ab368e7eb0393bc8cd884dbd9f5166d729becfef5cc17c16704ee6afa"))
+  (0.1 "https://github.com/cyclone-scheme/array-list/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
 )
