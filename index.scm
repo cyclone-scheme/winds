5e64d1be92c4119685bf9488b6753c88d4fdc6e0
Justin Ethier <justin.ethier@gmail.com>
2020-02-18 12:53:56 -0500

    Test using a different name

--- index.scm
+++ index.scm
@@ -1,6 +1,6 @@
 (cyclone-packages
  ((cyclone iset)
   (0.1 "https://raw.githubusercontent.com/arthurmaciel/cyclone-iset/master/package.scm" "https://github.com/arthurmaciel/cyclone-iset/archive/master.tar.gz" "6e7c5bc2e5aede85822ccfc0951e694321f0cc54841062397546c3d92460bb2b"))
- ((cyclone array-list)
+ (array-list
   (0.1 "https://github.com/cyclone-scheme/array-list/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
 )
