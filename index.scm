d6136d47e96935a394a27eb377a245592bafde21
arthurmaciel <arthurmaciel@gmail.com>
2020-02-23 00:53:16 -0300

    Update index.scm

--- index.scm
+++ index.scm
@@ -1,6 +1,6 @@
 (cyclone-packages
- ((cyclone iset)
-  (0.1 "https://raw.githubusercontent.com/arthurmaciel/cyclone-iset/master/package.scm" "https://github.com/arthurmaciel/cyclone-iset/archive/master.tar.gz" "6e7c5bc2e5aede85822ccfc0951e694321f0cc54841062397546c3d92460bb2b"))
  (array-list
   (0.1 "https://github.com/cyclone-scheme/array-list/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
+ (iset
+  (0.1 "https://github.com/cyclone-scheme/iset/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/iset/archive/v0.1.tar.gz" "6D87365E4A454219095DCC2D3736E1449ADDA746A6E1571582E49FFA47F8A4E5"))
 )
