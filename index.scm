7573f786d3208c86dc40b497a0d225f2975c95b8
arthurmaciel <arthurmaciel@gmail.com>
2020-02-23 02:06:17 -0300

    Update index.scm

--- index.scm
+++ index.scm
@@ -2,9 +2,9 @@
  (array-list
   (0.1 "https://github.com/cyclone-scheme/array-list/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
  (char-set
-  (0.1 "https://github.com/cyclone-scheme/char-set/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/char-set/archive/v0.1.tar.gz" "130DC1FBC9DA9998F035AE5260FBC1EE05A856FB7C7663CE823F74A13643ABD4"))
+  (0.1 "https://github.com/cyclone-scheme/char-set/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/char-set/archive/v0.1.tar.gz" "49CC8391DFC5E957B6036B7AA007B5FD1F922214DE03A8825A8CB842002AB784"))
  (iset
   (0.1 "https://github.com/cyclone-scheme/iset/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/iset/archive/v0.1.tar.gz" "6D87365E4A454219095DCC2D3736E1449ADDA746A6E1571582E49FFA47F8A4E5"))
  (string
-  (0.1 "https://github.com/cyclone-scheme/string/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/string/archive/v0.1.tar.gz" "10950D91F91CD0CCED8E1DD329051662C2A3C8F3E88E7E71F59D502CFE5C1BEC"))
+  (0.1 "https://github.com/cyclone-scheme/string/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/string/archive/v0.1.tar.gz" "B43A619B25D33A77121E392F923353F42F936B71EAE8CDEF771F580D700A24D0"))
 )
