ae64a91025bb887f69f39d66139cc554e7fcabd8
Justin Ethier <justin.ethier@gmail.com>
2020-02-24 13:45:15 -0500

    Added SRFI 26

--- index.scm
+++ index.scm
@@ -5,6 +5,8 @@
   (0.1 "https://github.com/cyclone-scheme/char-set/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/char-set/archive/v0.1.tar.gz" "49CC8391DFC5E957B6036B7AA007B5FD1F922214DE03A8825A8CB842002AB784"))
  (iset
   (0.1 "https://github.com/cyclone-scheme/iset/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/iset/archive/v0.1.tar.gz" "6D87365E4A454219095DCC2D3736E1449ADDA746A6E1571582E49FFA47F8A4E5"))
+ (srfi-26
+  (0.1 "https://github.com/cyclone-scheme/srfi-26/raw/master/package.scm" "https://github.com/cyclone-scheme/srfi-26/archive/v0.1.tar.gz" "F6E1B98E29743AD7ACD2743F2B2E4C8A2C595CC6672D26C4DA6E46D575D14FB7"))
  (string
   (0.1 "https://github.com/cyclone-scheme/string/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/string/archive/v0.1.tar.gz" "73945445AB0A5C607B501B84136E388F579F9DF6D1052F84FE880CD68B87BC6A"))
 )
