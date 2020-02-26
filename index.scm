4b63b8984cf3b02bfb1e0c9645c6eb4d6f4b6abf
arthurmaciel <arthurmaciel@gmail.com>
2020-02-25 23:46:32 -0300

    Added irregex...
    ...and changed index layout to improve readability.

--- index.scm
+++ index.scm
@@ -1,12 +1,26 @@
 (cyclone-packages
  (array-list
-  (0.1 "https://github.com/cyclone-scheme/array-list/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
+  (0.1 "https://github.com/cyclone-scheme/array-list/raw/v0.1/package.scm" 
+       "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" 
+       "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
  (char-set
-  (0.1 "https://github.com/cyclone-scheme/char-set/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/char-set/archive/v0.1.tar.gz" "49CC8391DFC5E957B6036B7AA007B5FD1F922214DE03A8825A8CB842002AB784"))
+  (0.1 "https://github.com/cyclone-scheme/char-set/raw/v0.1/package.scm" 
+       "https://github.com/cyclone-scheme/char-set/archive/v0.1.tar.gz" 
+       "49CC8391DFC5E957B6036B7AA007B5FD1F922214DE03A8825A8CB842002AB784"))
+ (irregex
+  (0.1 "https://github.com/cyclone-scheme/irregex/raw/v0.1/package.scm" 
+       "https://github.com/cyclone-scheme/irregex/archive/v0.1.tar.gz" 
+       "4f90280f2284c4086bc7ba97e644c581a73621291041a0d90abef8927f9f4c48"))
  (iset
-  (0.1 "https://github.com/cyclone-scheme/iset/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/iset/archive/v0.1.tar.gz" "6D87365E4A454219095DCC2D3736E1449ADDA746A6E1571582E49FFA47F8A4E5"))
+  (0.1 "https://github.com/cyclone-scheme/iset/raw/v0.1/package.scm" 
+       "https://github.com/cyclone-scheme/iset/archive/v0.1.tar.gz" 
+       "6D87365E4A454219095DCC2D3736E1449ADDA746A6E1571582E49FFA47F8A4E5"))
  (srfi-26
-  (0.1 "https://github.com/cyclone-scheme/srfi-26/raw/master/package.scm" "https://github.com/cyclone-scheme/srfi-26/archive/v0.1.tar.gz" "F6E1B98E29743AD7ACD2743F2B2E4C8A2C595CC6672D26C4DA6E46D575D14FB7"))
+  (0.1 "https://github.com/cyclone-scheme/srfi-26/raw/master/package.scm" 
+       "https://github.com/cyclone-scheme/srfi-26/archive/v0.1.tar.gz" 
+       "F6E1B98E29743AD7ACD2743F2B2E4C8A2C595CC6672D26C4DA6E46D575D14FB7"))
  (string
-  (0.1 "https://github.com/cyclone-scheme/string/raw/v0.1/package.scm" "https://github.com/cyclone-scheme/string/archive/v0.1.tar.gz" "73945445AB0A5C607B501B84136E388F579F9DF6D1052F84FE880CD68B87BC6A"))
+  (0.1 "https://github.com/cyclone-scheme/string/raw/v0.1/package.scm" 
+       "https://github.com/cyclone-scheme/string/archive/v0.1.tar.gz" 
+       "73945445AB0A5C607B501B84136E388F579F9DF6D1052F84FE880CD68B87BC6A"))
 )
