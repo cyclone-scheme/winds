ebe95b7729b5db7ded6e7abe621de4535faf96e1
Justin Ethier <justin.ethier@gmail.com>
2020-03-17 17:31:36 -0400

    New char-set release

--- index.scm
+++ index.scm
@@ -4,9 +4,9 @@
        "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" 
        "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
  (char-set
-  (0.1 "https://github.com/cyclone-scheme/char-set/raw/v0.1/package.scm" 
-       "https://github.com/cyclone-scheme/char-set/archive/v0.1.tar.gz" 
-       "49CC8391DFC5E957B6036B7AA007B5FD1F922214DE03A8825A8CB842002AB784"))
+  (0.2 "https://github.com/cyclone-scheme/char-set/raw/v0.2/package.scm" 
+       "https://github.com/cyclone-scheme/char-set/archive/v0.2.tar.gz" 
+       "534f0d0418eb63ec01f2bc788f970180f0ae4687a22c4c5f925805692b38bb8b"))
  (csv
   (0.1 "https://github.com/cyclone-scheme/csv/raw/v0.1/package.scm" 
        "https://github.com/cyclone-scheme/csv/archive/v0.1.tar.gz" 
