d96c8f019b77202119f1c37367a731e7e9bc22b9
Justin Ethier <justin.ethier@gmail.com>
2020-06-23 18:14:35 -0400

    New base64 release with updated docs

--- indexes/index.scm
+++ indexes/index.scm
@@ -4,9 +4,9 @@
        "https://github.com/cyclone-scheme/array-list/archive/v0.1.tar.gz" 
        "b621bed0e10093711af5f40563e4df3780ac189bcaa7c74b5c1f704d8ae8fcac"))
  (base64
-  (0.1 "https://github.com/cyclone-scheme/base64/raw/v0.1/package.scm"
-       "https://github.com/cyclone-scheme/base64/archive/v0.1.tar.gz" 
-       "ebcf317f62c66d7ede88cbb7b219c693143270b05ca658f3d87c0d2c8231c6d2"))
+  (0.2 "https://github.com/cyclone-scheme/base64/raw/v0.2/package.scm"
+       "https://github.com/cyclone-scheme/base64/archive/v0.2.tar.gz" 
+       "d04cf1dae30db95b105e39565b0c04ef9e03b02ac1370d17119198b7390e84e8"))
  (char-set
   (0.2 "https://github.com/cyclone-scheme/char-set/raw/v0.2/package.scm" 
        "https://github.com/cyclone-scheme/char-set/archive/v0.2.tar.gz" 
