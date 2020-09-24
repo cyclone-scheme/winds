c6bd51138b2efcdc02e4ad50cc82e8d13b4d47b3
Justin Ethier <justin.ethier@gmail.com>
2020-09-23 22:46:51 -0400

    Added srfi 197

--- indexes/index.scm
+++ indexes/index.scm
@@ -87,6 +87,10 @@
   (0.3 "https://github.com/cyclone-scheme/srfi-173/raw/master/package.scm" 
        "https://github.com/cyclone-scheme/srfi-173/archive/v0.3.tar.gz" 
        "33bcdf819b6d68ee66877503776ebb34891244c192601a3b8a0532d3c5012067"))
+ (srfi-197
+  (0.1 "https://github.com/cyclone-scheme/srfi-197/raw/master/package.scm" 
+       "https://github.com/cyclone-scheme/srfi-197/archive/v0.1.tar.gz" 
+       "94b82a83612215b804ed39a431f5781aaf757d8490f7ad3ecc52eb28c25d5754"))
  (string
   (0.3 "https://github.com/cyclone-scheme/string/raw/v0.3/package.scm" 
        "https://github.com/cyclone-scheme/string/archive/v0.3.tar.gz" 
