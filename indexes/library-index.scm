4d9ffeb4b53b57bbfb8dca642ffaa049d442d12b
cyclone-scheme <arthurmaciel@gmail.com>
2021-09-25 02:11:59 +0000

    Updated library-index.scm on push

--- indexes/library-index.scm
+++ indexes/library-index.scm
@@ -69,11 +69,6 @@
        char-set:empty
        char-set:ascii
        char-set:full))
-    ((cyclone char-set base)
-     (Char-Set
-       char-set?
-       immutable-char-set
-       char-set-contains?))
     ((cyclone char-set ascii)
      (char-set:lower-case
        char-set:upper-case
@@ -104,6 +99,11 @@
        char-set:symbol
        char-set:hex-digit
        char-set:blank))
+    ((cyclone char-set base)
+     (Char-Set
+       char-set?
+       immutable-char-set
+       char-set-contains?))
     ((cyclone char-set extras)
      (char-set
        ucs-range->char-set
@@ -452,44 +452,6 @@
            iset-cursor-next
            iset-ref
            end-of-iset?))
-        ((cyclone iset iterators)
-         (iset-empty?
-           iset-fold
-           iset-fold-node
-           iset-for-each
-           iset-for-each-node
-           iset->list
-           iset-size
-           iset=
-           iset<=
-           iset>=
-           iset-cursor
-           iset-cursor?
-           iset-cursor-next
-           iset-ref
-           end-of-iset?))
-        ((cyclone iset base)
-         (%make-iset
-           make-iset
-           iset?
-           iset-contains?
-           Integer-Set
-           iset-start
-           iset-end
-           iset-bits
-           iset-left
-           iset-right
-           iset-start-set!
-           iset-end-set!
-           iset-bits-set!
-           iset-left-set!
-           iset-right-set!))
-        ((cyclone iset optimize)
-         (iset-balance
-           iset-balance!
-           iset-optimize
-           iset-optimize!
-           iset->code))
         ((cyclone iset constructors)
          (iset iset-copy
                list->iset
@@ -508,7 +470,45 @@
                iset-copy-node
                iset-squash-bits!
                iset-insert-left!
-               iset-insert-right!))))
+               iset-insert-right!))
+        ((cyclone iset base)
+         (%make-iset
+           make-iset
+           iset?
+           iset-contains?
+           Integer-Set
+           iset-start
+           iset-end
+           iset-bits
+           iset-left
+           iset-right
+           iset-start-set!
+           iset-end-set!
+           iset-bits-set!
+           iset-left-set!
+           iset-right-set!))
+        ((cyclone iset iterators)
+         (iset-empty?
+           iset-fold
+           iset-fold-node
+           iset-for-each
+           iset-for-each-node
+           iset->list
+           iset-size
+           iset=
+           iset<=
+           iset>=
+           iset-cursor
+           iset-cursor?
+           iset-cursor-next
+           iset-ref
+           end-of-iset?))
+        ((cyclone iset optimize)
+         (iset-balance
+           iset-balance!
+           iset-optimize
+           iset-optimize!
+           iset->code))))
  (json (((cyclone json)
          (json-write json-read json->scm scm->json))))
  (md5 (((cyclone crypto md5) (md5))))
@@ -812,10 +812,10 @@
  (temple
    (((cyclone web temple)
      (render get-parse-tree build-parse-tree))
-    ((cyclone web temple trace)
-     (trace set-trace-level!))
     ((cyclone web temple parser)
-     (parse *read-size* string-pos))))
+     (parse *read-size* string-pos))
+    ((cyclone web temple trace)
+     (trace set-trace-level!))))
  (uri (((cyclone uri)
         (uri? uri->string
               make-uri
