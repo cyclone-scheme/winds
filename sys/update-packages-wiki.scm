9c3f6a172d3ccb7bd65fdd7042724e14a9be27d7
Arthur Maciel <arthurmaciel@gmail.com>
2022-06-30 22:15:33 -0300

    Metadata and wiki files are inside different dirs

--- sys/update-packages-wiki.scm
+++ sys/update-packages-wiki.scm
@@ -16,7 +16,7 @@
    "# Contributions from the community:\n\n"))
 
 (define (update-package-wiki! pkg-name)
-  (let* ((work-dir (->path *default-wiki-directory* pkg-name))
+  (let* ((work-dir (->path "." pkg-name))
          (doc-path (->path work-dir *default-doc-file*))
          (doc (if (file-exists? doc-path)
                   (file->string doc-path)
